4ed0ef8832d799f7de49fbb658eb664199dd2182
Ganraj Dol <84555910+ganrajdol99215@users.noreply.github.com>
2026-01-04 20:32:28 +0530

    Update KafkaUiApplication.java

--- app/src/main/java/com/demo/kafkaui/KafkaUiApplication.java
+++ app/src/main/java/com/demo/kafkaui/KafkaUiApplication.java
@@ -1,3 +1,6 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class KafkaUiApplication {
     public static void main(String[] args) {
